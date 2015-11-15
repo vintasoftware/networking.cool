c0dea713d2eeddbe9552df76b1e3c7e546341839
Filipe Ximenes <filipeximenes@gmail.com>
2015-11-14 20:45:34 -0500

    updating attendees json

--- process_attendees.py
+++ process_attendees.py
@@ -2,28 +2,48 @@
 
 with open('attendees.json', 'w+') as out_file:
     with open('attendees.html') as in_file:
-        out_file.write('[')
+
+        attendees = []
+
+        current = {}
+
+        # out_file.write('[')
 
         info_count = 0
         for line in in_file:
             if '<h3>' in line:
-                out_file.write('{"name": "' + line[4:-6] + '",')
-                out_file.write('\n')
+                current['name'] = line[4:-6]
+                # out_file.write('{"name": "' + line[4:-6] + '",')
+                # out_file.write('\n')
+
+            if '<img' in line:
+                out = line.split('src=')
+                current['image'] = out[1][1:-3]
+                # out_file.write('"image": ' + out[1] + '",')
 
             if 'attendee-info' in line:
                 info_count += 1
                 out = line[27:-7]
                 if out.endswith(','):
                     out = out[:-1]
-                out_file.write('"info' + str(info_count) + '": "' + out + '",')
-                out_file.write('\n')
+                current['info' + str(info_count)] = out
+
+                # out_file.write('"info' + str(info_count) + '": "' + out + '",')
+                # out_file.write('\n')
 
             if 'attendee-country' in line:
-                out_file.write('"country": "' + line[30:-7] + '"},')
-                out_file.write('\n')
-                out_file.write('\n')
+                # out_file.write('"country": "' + line[30:-7] + '"},')
+                # out_file.write('\n')
+                # out_file.write('\n')
                 info_count = 0
+                current['country'] = line[30:-7]
+                attendees.append(current)
 
-        out_file.write(']')
+                current = {}
 
+        # out_file.write(']')
+
+
+    import json
+    out_file.write(json.dumps(attendees, sort_keys=True, indent=2, separators=(',', ': ')))
 
