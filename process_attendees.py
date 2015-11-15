106d10c5ef41e6caeb53922de1b9e79a9259a132
Filipe Ximenes <filipeximenes@gmail.com>
2015-11-14 20:53:04 -0500

    adding attendees by name json

--- process_attendees.py
+++ process_attendees.py
@@ -1,49 +1,45 @@
 
 
-with open('attendees.json', 'w+') as out_file:
-    with open('attendees.html') as in_file:
+with open('attendees.html') as in_file:
+    attendees = []
+    attendees_by_name = {}
+    current = {}
 
-        attendees = []
+    info_count = 0
+    for line in in_file:
+        if '<h3>' in line:
+            current['name'] = line[4:-6]
 
-        current = {}
+        if '<img' in line:
+            out = line.split('src=')
+            current['image'] = out[1][1:-3]
 
-        # out_file.write('[')
-
-        info_count = 0
-        for line in in_file:
-            if '<h3>' in line:
-                current['name'] = line[4:-6]
-                # out_file.write('{"name": "' + line[4:-6] + '",')
-                # out_file.write('\n')
-
-            if '<img' in line:
-                out = line.split('src=')
-                current['image'] = out[1][1:-3]
-                # out_file.write('"image": ' + out[1] + '",')
-
-            if 'attendee-info' in line:
-                info_count += 1
-                out = line[27:-7]
-                if out.endswith(','):
-                    out = out[:-1]
-                current['info' + str(info_count)] = out
-
-                # out_file.write('"info' + str(info_count) + '": "' + out + '",')
-                # out_file.write('\n')
-
-            if 'attendee-country' in line:
-                # out_file.write('"country": "' + line[30:-7] + '"},')
-                # out_file.write('\n')
-                # out_file.write('\n')
-                info_count = 0
-                current['country'] = line[30:-7]
-                attendees.append(current)
-
-                current = {}
-
-        # out_file.write(']')
+        if 'attendee-info' in line:
+            info_count += 1
+            out = line[27:-7]
+            if out.endswith(','):
+                out = out[:-1]
+            current['info' + str(info_count)] = out
 
 
-    import json
-    out_file.write(json.dumps(attendees, sort_keys=True, indent=2, separators=(',', ': ')))
+        if 'attendee-country' in line:
+            info_count = 0
+            current['country'] = line[30:-7]
+            attendees.append(current)
 
+            attendees_by_name[current['name']] = current
+
+            current = {}
+
+import json
+
+out_file = open('attendees.json', 'w+')
+out_file.write(json.dumps(attendees, sort_keys=True, indent=2, separators=(',', ': ')))
+out_file.close()
+
+out_file1 = open('attendees_by_name.json', 'w+')
+out_file1.write(json.dumps(attendees_by_name, sort_keys=True, indent=2, separators=(',', ': ')))
+out_file1.close()
+
+print len(attendees)
+print len(attendees_by_name.items())
